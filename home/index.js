88056fed26cf7a3600c3834810b1018c0035a4e0
Szymixjestkozak <szymi.2285.pl@outlook.com>
2025-06-15 18:26:50 +0200

    Initial commit

--- home/index.js
+++ home/index.js
@@ -1,14 +1,1 @@
-const allLinks = document.querySelectorAll("a");
-allLinks.forEach((link) => {
-link.addEventListener("click", (event) => {
-if (!document.startViewTransition) {
-setActiveItem(event.target);
-return;
-}
-document.startViewTransition(() => setActiveItem(event.target));
-});
-});
-function setActiveItem(element) {
-allLinks.forEach((link) => link.classList.remove("active"));
-element.classList.add("active");
-}
+//javascript
